e76f86e416f395374df5e78835af57098d668e04
stregie <stregie@outlook.com>
2021-03-03 00:24:16 +0100

    Minor changes on demosite, Learning JavaScript has started

---
+++ 06-javascript/js/01-hello-world.js
@@ -0,0 +1,39 @@
+//your apples:
+var apple1 = 5;
+
+function my
+
+function fa (){
+	console.log("fa happened.");
+}
+fa();
+console.log(fa);
+
+var fb = function (){
+	console.log("fb happened");
+	return 4;
+}
+fb();
+console.log(fb);
+
+
+
+// var a = "Hello World!";
+
+// function add (x,y){
+// 	var result = x + y;
+// 	console.log(result);
+// }
+// function subtract (x,y){
+// 	var result = x - y;
+// 	console.log(result);
+// }
+// function add2 (x,y){
+// 	return x + y;
+// }
+
+// add (2,3);
+// subtract (5,2);
+// var b = add2(4,5);
+// console.log(b);
+// console.log(add2(1,2));
